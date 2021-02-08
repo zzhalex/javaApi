f223419b03b36cb8a10d95f22a21ab0ff5b92bfd
Zhihao Zhou <zzh001xx@hotmail.com>
2021-02-07 16:05:33 -0800

    first commit

---
+++ src/main/java/com/example/demo/dao/PersonDataAccessService.java
@@ -0,0 +1,41 @@
+// package com.example.demo.dao;
+
+// import java.util.List;
+// import java.util.Optional;
+// import java.util.UUID;
+
+// import com.example.demo.model.Person;
+
+// public class PersonDataAccessService implements PersonDao {
+
+// @Override
+// public int deletePersonById(UUID id) {
+// // TODO Auto-generated method stub
+// return 0;
+// }
+
+// @Override
+// public int insertPerson(UUID id, Person person) {
+// // TODO Auto-generated method stub
+// return 0;
+// }
+
+// @Override
+// public List<Person> selectAllPeople() {
+// // TODO Auto-generated method stub
+// return List.of(new Person(UUID.randomUUID(), "FROM POSTGRES DB"));
+// }
+
+// @Override
+// public Optional<Person> selectPersonById(UUID id) {
+// // TODO Auto-generated method stub
+// return null;
+// }
+
+// @Override
+// public int updatePersonById(UUID id, Person person) {
+// // TODO Auto-generated method stub
+// return 0;
+// }
+
+// }
